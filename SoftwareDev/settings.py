2bb186bf49298a85754945bb02da7d61dc4f6a12
milokhiggins <milokhiggins@gmail.com>
2021-02-12 19:07:55 +0000

    added log file config

--- SoftwareDev/settings.py
+++ SoftwareDev/settings.py
@@ -121,3 +121,23 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/app-logs/django.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
