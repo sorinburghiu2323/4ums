792bf49ecdb3646261108189c6ab418c1de20cd6
Oliver Baker <oijbaker@outlook.com>
2021-02-17 18:00:14 +0000

    Created navbar

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="nav">
+        <a href="/communtites"><div id="communities" v-on:click="goTo('communities')">Communities</div></a>   
+        <a href="/profile"><div id="profile" v-on:click="goTo('profile')">Profile</div></a>
+        <a href="/leaderboard"><div id="leaderboard" v-on:click="goTo('leaderboard')">Leaderboard</div></a>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Navbar",
+    data: function() {
+        return {
+            pages: ["communities", "profile", "leaderboard"]
+        }
+    },
+    methods: {
+        goTo: function(location) {
+            // update the text colour depending on which is selected
+            document.getElementById(location).style.color = "black";
+            for (var i=0; i<3; i++) {
+                if (this.pages[i] != location) {
+                    document.getElementById(this.pages[i]).style.color = "white";
+                }
+            }
+        }
+    }
+}
+</script>
+
+<style>
+#nav {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 10vh;
+    background-color: grey;
+    color:white;
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+}
+</style>
