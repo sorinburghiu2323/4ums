ae2805c335ef0619811fe903b3852b736b1c44d0
Oliver Baker <oijbaker@outlook.com>
2021-02-19 11:41:37 +0000

    Update styling again

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="nav">
-        <router-link class="nav-link" to="../" v-on:click = "goTo('homepage')"><div id="homepage" v-on:click="goTo('homepage')"><font-awesome-icon :icon="['fas', 'home']" /></div></router-link>
-        <router-link class="nav-link" to="../communities" v-on:click = "goTo('communities')"><div id="communities" v-on:click="goTo('communities')"><font-awesome-icon :icon="['fas', 'users']" /></div></router-link> 
-        <router-link class="nav-link" to="../profile" v-on:click = "goTo('profile')"><div id="profile" v-on:click="goTo('profile')"><font-awesome-icon :icon="['fas', 'user']" /></div></router-link>
-        <router-link class="nav-link" to="../leaderboard" v-on:click = "goTo('leaderboard')"><div id="leaderboard" v-on:click="goTo('leaderboard')"><font-awesome-icon :icon="['fas', 'trophy']" /></div></router-link>
+        <router-link class="nav-link" to="../" v-on:click = "goTo('homepage')"><div id="homepage" v-on:click="goTo('homepage')"><font-awesome-icon :icon="['fas', 'home']" /><p class="icon">Feed</p></div></router-link>
+        <router-link class="nav-link" to="../communities" v-on:click = "goTo('communities')"><div id="communities" v-on:click="goTo('communities')"><font-awesome-icon :icon="['fas', 'users']" /><p class="icon">Communities</p></div></router-link> 
+        <router-link class="nav-link" to="../profile" v-on:click = "goTo('profile')"><div id="profile" v-on:click="goTo('profile')"><font-awesome-icon :icon="['fas', 'user']" /><p class="icon">Profile</p></div></router-link>
+        <router-link class="nav-link" to="../leaderboard" v-on:click = "goTo('leaderboard')"><div id="leaderboard" v-on:click="goTo('leaderboard')"><font-awesome-icon :icon="['fas', 'trophy']" /><p class="icon">Leaderboard</p></div></router-link>
     </div>
 </template>
 
@@ -23,14 +23,15 @@ export default {
             // this.$router.replace(location)
             for (var i=0; i<4; i++) {
                 if (this.pages[i] != location) {
-                    document.getElementById(this.pages[i]).style.color = "white";
+                    document.getElementById(this.pages[i]).style.color = "rgb(126, 126, 126)";
                 }
             }
         }
     },
     mounted() {
-        for (var i=0; i<4; i++) {
-            document.getElementById(this.pages[i]).style.color = "white";
+        document.getElementById('homepage').style.color = "#5FF9AB";
+        for (var i=1; i<4; i++) {
+            document.getElementById(this.pages[i]).style.color = "rgb(126, 126, 126)";
         }
     }
 }
@@ -38,17 +39,24 @@ export default {
 
 <style>
 #nav {
+    font-size: 4vh;
     font-family: 'Trebuchet MS';
     display: flex;
     justify-content: space-evenly;
     align-items: center;
-    height: 10vh;
+    height: 11vh;
     background-color: #161626;
-    color:white;
+    color:rgb(126, 126, 126);
     width: 100%;
     position: fixed;
     bottom: 0;
     left: 0;
     border-radius: 25px;
+}
+
+.icon {
+    font-size: 2vh;
+    margin-top: 1vh;
+    margin-bottom: 1vh;
 }
 </style>
