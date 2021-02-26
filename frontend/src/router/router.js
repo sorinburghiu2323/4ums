e6f98ebdc2f7cd853beba5374b033ff384007375
Will <williamorringe@gmail.com>
2021-02-26 14:01:22 +0000

    initial commit and removed # from url

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -74,6 +74,7 @@ const routes = [
 ];
 
 const router = new VueRouter({
+    mode: 'history',
     routes
 })
 
---
+++ frontend/src/views/PostPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="infinite" class="container">
+    <div class="header">
+      <h1>Feed</h1>
+      <div class="settings-icon">
+        <font-awesome-icon :icon="['fas', 'cog']"></font-awesome-icon>
+      </div>
+    </div>
+    <div class="search-section">
+      <input placeholder="Search for a thread..." type="text"/>
+      <div class="search-icon">
+        <font-awesome-icon :icon="['fas', 'search']"></font-awesome-icon>
+      </div>
+    </div>
+    <div v-if="loadedPosts">
+      <div class="post-content">
+        <Post v-for="(post, index) in allPosts" :key="index"
+              :post="post"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Post from "@/components/posts/Post";
+
+export default {
+  name: "PostPage",
+  components: {
+    Post,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
