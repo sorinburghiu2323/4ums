c43955a3b4aa607fc75dc3f8c793542e16b1b52e
milokhiggins <milokhiggins@gmail.com>
2021-03-05 17:55:24 +0000

    add util for generating email + recording reset code

---
+++ backend/Utils/password_reset.py
@@ -0,0 +1,9 @@
+
+from SoftwareDev import settings
+
+import base64
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+#see Zoho mail - forever free tier (very basic but enough)
