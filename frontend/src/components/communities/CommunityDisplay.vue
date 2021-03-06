1016cb7c08d8ad4143b65853f3e5b731791b7648
Ollie Baker <oijbaker@outlook.com>
2021-03-06 12:55:51 +0000

    Leaving a community

--- frontend/src/components/communities/CommunityDisplay.vue
+++ frontend/src/components/communities/CommunityDisplay.vue
@@ -7,12 +7,18 @@
       </div>
 
       <div v-if="editMode" class="editBtn">
-        <button v-if="communityType === 'memberof'"
-                class="leave" @click.stop.prevent="leaveCommunity()">
+        <button
+          v-if="communityType === 'memberof'"
+          class="leave"
+          @click.stop.prevent="leaveCommunity()"
+        >
           Leave
         </button>
-        <button v-if="communityType === 'created'"
-                class="delete" @click.stop.prevent="deleteCommunity()">
+        <button
+          v-if="communityType === 'created'"
+          class="delete"
+          @click.stop.prevent="deleteCommunity()"
+        >
           Delete
         </button>
       </div>
@@ -21,38 +27,43 @@
         <p>{{ community.description }}</p>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
 export default {
-    name: 'CommunityDisplay',
-    props: {
-        community: Object,
-        communityType: String,
-        editMode: {
-            default: false,
-            type: Boolean,
-        }
+  name: "CommunityDisplay",
+  props: {
+    community: Object,
+    communityType: String,
+    editMode: {
+      default: false,
+      type: Boolean,
     },
-    methods: {
-        navigateToCommunity() {
-            this.$router.push({
-                name:'Community',
-                params:{
-                    id: this.community.id
-                }
-            });
+  },
+  methods: {
+    navigateToCommunity() {
+      this.$router.push({
+        name: "Community",
+        params: {
+          id: this.community.id,
         },
-        leaveCommunity() {
-            this.$root.$emit('updateCommunities');
+      });
+    },
+    leaveCommunity() {
+      this.$router.push({
+        name: "Leave",
+        params: {
+          id: this.community.id,
         },
-        deleteCommunity() {
-            this.$root.$emit('updateCommunities');
-        }
-    }
-}
+      });
+      this.$root.$emit("updateCommunities");
+    },
+    deleteCommunity() {
+      this.$root.$emit("updateCommunities");
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -66,7 +77,12 @@ export default {
   border: none;
   border-radius: 25px;
   background: rgb(40, 44, 58);
-  background: linear-gradient(90deg, rgba(40, 44, 58, 1) 0%, rgba(27, 30, 40, 1) 35%, rgba(8, 9, 11, 1) 100%);
+  background: linear-gradient(
+    90deg,
+    rgba(40, 44, 58, 1) 0%,
+    rgba(27, 30, 40, 1) 35%,
+    rgba(8, 9, 11, 1) 100%
+  );
 }
 .color-box {
   height: 108px;
@@ -76,16 +92,28 @@ export default {
 }
 
 .all {
-    background: rgb(254,155,47);
-    background: linear-gradient(90deg, rgba(254,155,47,1) 0%, rgba(254,101,15,1) 35%);
+  background: rgb(254, 155, 47);
+  background: linear-gradient(
+    90deg,
+    rgba(254, 155, 47, 1) 0%,
+    rgba(254, 101, 15, 1) 35%
+  );
 }
 .memberof {
   background: rgb(52, 235, 233);
-  background: linear-gradient(270deg, rgba(52, 235, 233, 1) 0%, rgba(101, 255, 167, 1) 35%);
+  background: linear-gradient(
+    270deg,
+    rgba(52, 235, 233, 1) 0%,
+    rgba(101, 255, 167, 1) 35%
+  );
 }
 .created {
-    background: rgb(253,248,98);
-    background: linear-gradient(270deg, rgb(253,248,98) 0%, rgba(255,237,0,1) 35%);
+  background: rgb(253, 248, 98);
+  background: linear-gradient(
+    270deg,
+    rgb(253, 248, 98) 0%,
+    rgba(255, 237, 0, 1) 35%
+  );
 }
 
 .details {
@@ -96,55 +124,63 @@ export default {
 }
 
 .details p {
-    margin: 0;
+  margin: 0;
 }
 
 .details .title {
-    font-size: 20px;
-    height: 30%;
-    margin-bottom: 4px;
-    display: flex;
-    justify-content: space-between;
-    width: 80%;
+  font-size: 20px;
+  height: 30%;
+  margin-bottom: 4px;
+  display: flex;
+  justify-content: space-between;
+  width: 80%;
 }
 
 .details .description {
-    height: 70%;
-    font-size: 15px;
-    color: #7E7E7E;
+  height: 70%;
+  font-size: 15px;
+  color: #7e7e7e;
 }
 
 .description p {
-    overflow: hidden;
-   text-overflow: ellipsis;
-   display: -webkit-box;
-   -webkit-line-clamp: 2; /* number of lines to show */
-   -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2; /* number of lines to show */
+  -webkit-box-orient: vertical;
 }
 
-.editBtn{
-    position: absolute;
-    top: -10px;
-    right: 20px;
+.editBtn {
+  position: absolute;
+  top: -10px;
+  right: 20px;
 }
 
 .editBtn button {
-    width: 100px;
-    height: 25px;
-    border-radius: 25px;;
-    text-align: center;
-    font-weight: 600;
-    border: none;
-    outline: none;
-    cursor: pointer;
+  width: 100px;
+  height: 25px;
+  border-radius: 25px;
+  text-align: center;
+  font-weight: 600;
+  border: none;
+  outline: none;
+  cursor: pointer;
 }
 
 .leave {
-    background: linear-gradient(270deg, rgba(52,235,233,1) 0%, rgba(101,255,167,1) 35%);
+  background: linear-gradient(
+    270deg,
+    rgba(52, 235, 233, 1) 0%,
+    rgba(101, 255, 167, 1) 35%
+  );
 }
 
 .delete {
-    background: rgb(254,155,47);
-    background: linear-gradient(90deg, rgba(254,155,47,1) 0%, rgba(254,101,15,1) 35%);
+  background: rgb(254, 155, 47);
+  background: linear-gradient(
+    90deg,
+    rgba(254, 155, 47, 1) 0%,
+    rgba(254, 101, 15, 1) 35%
+  );
 }
 </style>
--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,114 +1,119 @@
 import CommunitiesPage from "../views/CommunitiesPage.vue";
+import Community from "../views/Community.vue";
 import CreateCommunity from "../views/CreateCommunity.vue";
 import CreatePost from "../views/CreatePost.vue";
+import Feed from "../views/Feed.vue";
 import Leaderboard from "../views/Leaderboard.vue";
+import LeaveCommunity from "../views/LeaveCommunity.vue";
 import LoginPage from "../views/LoginPage.vue";
+import ManageCommunities from "../views/ManageCommunities.vue";
+import NotFound from "@/views/NotFound";
+import PostPage from "@/views/PostPage";
 import PreviewCommunity from "../views/PreviewCommunity.vue";
 import Profile from "../views/Profile.vue";
 import RegisterPage from "../views/RegisterPage.vue";
-import Community from "../views/Community.vue"
-import ManageCommunities from "../views/ManageCommunities.vue"
+import User from "@/views/User";
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Feed from '../views/Feed.vue'
-import axios from 'axios'
-import PostPage from "@/views/PostPage";
-import NotFound from "@/views/NotFound";
-import User from "@/views/User";
+import axios from "axios";
 
 Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: "/",
-        name: "Feed",
-        component: Feed,
-    },
-    {
-        path: "/communities/post/:type",
-        name: "CreatePost",
-        component: CreatePost,
-    },
-    {
-        path: "/login",
-        name: "LoginPage",
-        component: LoginPage,
-    },
-    {
-        path: "/register",
-        name: "RegisterPage",
-        component: RegisterPage,
-    },
-    {
-        path: "/communities",
-        name: "Communities",
-        component: CommunitiesPage,
-    },
-    {
-        path: "/profile",
-        name: "Profile",
-        component: Profile,
-    },
-    {
-        path: "/leaderboard",
-        name: "Leaderboard",
-        component: Leaderboard,
-    },
-    {
-        path: "/communities/create",
-        name: "CreateCommunity",
-        component: CreateCommunity,
-    },
-    {
-        path: "/communities/create/preview",
-        name: "PreviewCommunity",
-        component: PreviewCommunity,
-    },
-    {
-        path: "/communities/:id/posts/:postId",
-        name: "PostPage",
-        component: PostPage,
-    },
-    {
-        path: '/communities/:id',
-        name: 'Community',
-        component: Community,
-    },
-    {
-        path: '/users/:id',
-        name: 'User',
-        component: User,
-    },
-    {
-        path: '/manage',
-        name: 'Manage',
-        component: ManageCommunities,
-    },
-    {
-        path: '*',
-        name: 'NotFound',
-        component: NotFound,
-    }
+  {
+    path: "/",
+    name: "Feed",
+    component: Feed,
+  },
+  {
+    path: "/communities/post/:type",
+    name: "CreatePost",
+    component: CreatePost,
+  },
+  {
+    path: "/login",
+    name: "LoginPage",
+    component: LoginPage,
+  },
+  {
+    path: "/register",
+    name: "RegisterPage",
+    component: RegisterPage,
+  },
+  {
+    path: "/communities",
+    name: "Communities",
+    component: CommunitiesPage,
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: Profile,
+  },
+  {
+    path: "/leaderboard",
+    name: "Leaderboard",
+    component: Leaderboard,
+  },
+  {
+    path: "/communities/create",
+    name: "CreateCommunity",
+    component: CreateCommunity,
+  },
+  {
+    path: "/communities/create/preview",
+    name: "PreviewCommunity",
+    component: PreviewCommunity,
+  },
+  {
+    path: "/communities/:id/posts/:postId",
+    name: "PostPage",
+    component: PostPage,
+  },
+  {
+    path: "/communities/:id",
+    name: "Community",
+    component: Community,
+  },
+  {
+    path: "/users/:id",
+    name: "User",
+    component: User,
+  },
+  {
+    path: "/manage",
+    name: "Manage",
+    component: ManageCommunities,
+  },
+  {
+    path: "/leave/:id",
+    name: "Leave",
+    component: LeaveCommunity,
+  },
+  {
+    path: "*",
+    name: "NotFound",
+    component: NotFound,
+  },
 ];
 
 const router = new VueRouter({
-    mode: 'history',
-    routes
-})
+  mode: "history",
+  routes,
+});
 
 router.beforeEach((to, from, next) => {
-    if (to.name !== from.name) {
-        // Direct user to login if they are not authenticated
-        axios.get('/api/communities?type=other')
-            .catch((err) => {
-                console.error(err);
-                if (to.path !== '/login' && to.path !== '/register') {
-                    return next({
-                        path: '/login',
-                    });
-                }
-            });
-    }
-    next();
+  if (to.name !== from.name) {
+    // Direct user to login if they are not authenticated
+    axios.get("/api/communities?type=other").catch((err) => {
+      console.error(err);
+      if (to.path !== "/login" && to.path !== "/register") {
+        return next({
+          path: "/login",
+        });
+      }
+    });
+  }
+  next();
 });
-export default router
+export default router;
