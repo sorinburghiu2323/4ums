5b6a94379dacecc0f9701e1e6607963576aa0174
Ollie Baker <oijbaker@outlook.com>
2021-03-07 11:36:55 +0000

    Implemented user page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,68 +1,78 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router/router.js";
-// Fontawesome documentation: https://www.npmjs.com/package/@fortawesome/vue-fontawesome
-// Regular style icons
-import {library} from "@fortawesome/fontawesome-svg-core";
-// Brand style icons
-import {faFontAwesome} from "@fortawesome/free-brands-svg-icons";
-// Solid style icons
 import {
   faAlignLeft,
   faArrowLeft,
   faCheckCircle,
   faCog,
   faCommentDots,
+  faCrown,
+  faEdit,
   faHome,
+  faMedal,
   faPaperPlane,
   faQuestion,
   faQuestionCircle,
   faSearch,
+  faShareSquare,
   faSort,
+  faStar,
   faThumbsUp,
+  faTimes,
   faTimesCircle,
   faTrophy,
   faUser,
-  faUsers,
   faUserSecret,
-  faTimes,
-  faMedal,
-  faCrown,
+  faUsers,
 } from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+
+import App from "./App.vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import Vue from "vue";
+import { faFontAwesome } from "@fortawesome/free-brands-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import router from "./router/router.js";
+
+// Fontawesome documentation: https://www.npmjs.com/package/@fortawesome/vue-fontawesome
+// Regular style icons
+
+// Brand style icons
+
+// Solid style icons
 
 // Add icons here after import
 library.add(
-    faUserSecret,
-    faSort,
-    faFontAwesome,
-    faSearch,
-    faCog,
-    faTrophy,
-    faUsers,
-    faUser,
-    faHome,
-    faQuestion,
-    faAlignLeft,
-    faTimesCircle,
-    faCommentDots,
-    faArrowLeft,
-    faThumbsUp,
-    faQuestionCircle,
-    faCheckCircle,
-    faPaperPlane,
-    faTimes,
-    faMedal,
-    faCrown,
+  faUserSecret,
+  faSort,
+  faFontAwesome,
+  faSearch,
+  faCog,
+  faTrophy,
+  faUsers,
+  faUser,
+  faHome,
+  faQuestion,
+  faAlignLeft,
+  faTimesCircle,
+  faCommentDots,
+  faArrowLeft,
+  faThumbsUp,
+  faQuestionCircle,
+  faCheckCircle,
+  faPaperPlane,
+  faTimes,
+  faMedal,
+  faCrown,
+  faEdit,
+  faShareSquare,
+  faStar
 );
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.config.devtools = true;
 Vue.config.productionTip = false;
 
 // Cookie library
-var VueCookie = require('vue-cookie');
+var VueCookie = require("vue-cookie");
 Vue.use(VueCookie);
 new Vue({
   router,
