a994c8037110c51524884d2dd022330fdb137862
milokhiggins <milokhiggins@gmail.com>
2021-03-08 11:36:36 +0000

    complete email sending code

--- backend/Utils/password_reset.py
+++ backend/Utils/password_reset.py
@@ -12,10 +12,13 @@ from email.mime.multipart import MIMEMultipart
 from email import message_from_string
 
 from googleapiclient.errors import HttpError
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
 
 # boto is used *ONLY* in deployed app to access secrets
 if "DJANGO-AWS-4UMS-DEPLOYED" in environ_variables:
     import boto3
+    from botocore.exceptions import ClientError
 
 
 def create_message(sender, to, subject, message_text, message_html):
@@ -58,12 +61,12 @@ def send_message(service, user_id, message):
     except HttpError as error:
         logger = logging.getLogger("root")
         logger.error(
-            "HTTP error occured when attempting to call the Gmail API"
+            "HTTP error occured when calling the Gmail API:"
         )
         logger.error(str(error))
 
 
-def build_password_reset_email(email_addr, user_id, reset_code, first_name, username):
+def build_email(email_addr, user_id, reset_code, first_name, username):
     """
     Build the password reset email.
 
@@ -105,7 +108,6 @@ def send_reset_email(message):
 
     if "DJANGO-AWS-4UMS-DEPLOYED" not in environ_variables:
         # don't send the email unless we're in live deployment
-
         # for testing purposes: decode plain text and print to console
         msg_b64 = message['raw']
         msg_str = urlsafe_b64decode(msg_b64.encode()).decode()
@@ -117,13 +119,42 @@ def send_reset_email(message):
                 break
         return
 
+
     session = boto3.session.Session()
     client = session.client(
         service_name="secretsmanager", region_name="us-west-2"
     )
-    #this isn't ROBUST enough
-    #add error catching (don't do anything though, just log the error)
-    creds_secret = client.get_secret_value(SecretId="4ums/email-creds")
-    creds_raw = creds_secret['SecretString']
 
-    #process email creds here
+    try:
+        secret_value = client.get_secret_value(SecretId="4ums/email-creds")
+    except ClientError as e:
+        logger = logging.getLogger("root")
+        if e.response['Error']['Code'] == 'ResourceNotFoundException':
+            logger.error(
+                "Failed to send email - Couldn't get secret '4ums/email-creds'"
+            )
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            logger.error(
+                f"Failed to send email - secret request was invalid due to: {e}"
+            )
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            logger.error(
+                f"Failed to send email - secret request had invalid params: {e}"
+            )
+        return
+
+    creds_raw = secret_value['SecretString']
+    creds = pickle.loads(creds_raw)
+
+    if not creds.valid:
+        if creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            #credentials not usable; can't send email
+            logger = logging.getLogger("root")
+            logger.error(
+                "Failed to send email - Gmail API credentials are invalid"
+            )
+
+    service = build('gmail', 'v1', credentials=creds)
+    send_message(service, "me", message)
