3beeb91ac93a1b78dc8fcf3db869ef5b7f19bb86
Ollie Baker <oijbaker@outlook.com>
2021-03-09 19:43:47 +0000

    console.log

--- frontend/src/views/ChangePassword.vue
+++ frontend/src/views/ChangePassword.vue
@@ -126,7 +126,6 @@ export default {
             })
             .catch((error) => {
               this.errMessage = error.response.data;
-              console.log(error);
             });
         })
         .catch((error) => {
